d8537e060c9a98adaf5e6757c3c5e9542c48a054
warriormaster12 <streng.alexander@outlook.com>
2024-06-08 00:15:36 +0300

    Improve logger

--- cu-engine/src/public/logger.h
+++ cu-engine/src/public/logger.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <fmt/core.h>
 #include <fmt/color.h>
+#include <fmt/chrono.h>
 
-#define ENGINE_INFO(...) ::fmt::print(fg(fmt::color::lawn_green), fmt::format(__VA_ARGS__) + "\n");
-#define ENGINE_WARN(...) ::fmt::print(fg(fmt::color::yellow), fmt::format(__VA_ARGS__) + "\n");
-#define ENGINE_ERROR(...) ::fmt::print(fg(fmt::color::crimson), fmt::format(__VA_ARGS__) + "\n");
+#define ENGINE_INFO(...) ::fmt::print(fg(fmt::color::lawn_green), "[{:%H:%M}]: {}\n", std::chrono::system_clock::now(), fmt::format(__VA_ARGS__))
+#define ENGINE_WARN(...) ::fmt::print(fg(fmt::color::yellow), "[{:%H:%M}]: {}\n", std::chrono::system_clock::now(), fmt::format(__VA_ARGS__))
+#define ENGINE_ERROR(...) ::fmt::print(fg(fmt::color::crimson), "[{:%H:%M}]: {}\n", std::chrono::system_clock::now(), fmt::format(__VA_ARGS__))
